a592e9bb5e4443f939f4bfe622d7bde7b7b1c1c7
vesplah <rjjudge13@gmail.com>
2014-08-29 14:15:46 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,19 @@
+//javac HelloWorld.java
+////
+//java HelloWorld//
+/////
+  public class HelloWorld{
+    
+    public static void main(String[] args) {
+     System.out.println("##########################");
+     System.out.println("##########################");
+     System.out.println("#                        #");
+     System.out.println("#     Hello World 2014   #");
+     System.out.println("#                        #");
+     System.out.println("##########################");
+     System.out.println("##########################");
+     System.out.println("testing");
+     
+          }
+  
+}
