af9d4b964f4a6c29c0e50e93cd7671a0c0bedfff
vesplah <rjjudge13@gmail.com>
2014-09-01 23:33:59 +0000

    finished hw01

---
+++ hw01/HelloClass.java
@@ -0,0 +1,20 @@
+///comment
+
+ public class HelloClass{
+     
+     public static void main(String[] args) {
+    
+     System.out.println("------------");
+     System.out.println("| WELCOME |"); 
+     System.out.println("------------");
+     System.out.println(" ^  ^  ^  ^  ^  ^");
+     System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\");
+     System.out.println("|-R--J--J--2--1--8-|");
+     System.out.println("\\ /\\ /\\ /\\ /\\ /\\ /");
+     System.out.println(" v  v  v  v  v  v");
+     //
+     System.out.println("I like dogs.  Especially the fluffy ones.");
+     System.out.println("Not so much for the unfluffy ones.");
+    
+     }
+ }
