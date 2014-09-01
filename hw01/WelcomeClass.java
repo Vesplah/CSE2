84b7e949c415766e1355d3a2640722cdd36b7bb7
vesplah <rjjudge13@gmail.com>
2014-09-01 23:45:41 +0000

    The hw01 with correct names

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,24 @@
+///Ryan Judge
+// 1 September 2014
+//Welcome Class Java Program
+
+//define a class
+ public class WelcomeClass{
+     //add main method
+     public static void main(String[] args) {
+    
+    //print statement
+     System.out.println("------------");
+     System.out.println("| WELCOME |"); 
+     System.out.println("------------");
+     System.out.println(" ^  ^  ^  ^  ^  ^");
+     System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\");
+     System.out.println("|-R--J--J--2--1--8-|");
+     System.out.println("\\ /\\ /\\ /\\ /\\ /\\ /");
+     System.out.println(" v  v  v  v  v  v");
+     //
+     System.out.println("I like dogs.  Especially the fluffy ones.");
+     System.out.println("Not so much for the unfluffy ones.");
+    
+     }
+ }
